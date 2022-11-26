52851d0a955e399453ac99c690f9d653022dd452
iriverosu <69812493+iriverosu@users.noreply.github.com>
2022-11-26 15:05:42 -0500

    r

--- script/PS4.R
+++ script/PS4.R
@@ -1,6 +1,10 @@
 
 # Cargamos los paquetes
+install.packages(("rvest"))
+library(rvest)
 library(pacman)
+install.packages(("tm"))
+library(tm)
 p_load(tidyverse, rstudioapi)
 p_load("stopwords", "stringi", "tm", "rvest")
 
@@ -8,10 +12,10 @@ p_load("stopwords", "stringi", "tm", "rvest")
 setwd(dirname(getActiveDocumentContext()$path))
 
 # Cargamos los datos
-tt <- read.csv("data/tweets.csv")
+train <- read.csv("data/tweets.csv")
 
 # Visualizamos las primeras filas
-head(tt)
+head(train)
 
 # Estudiamos la estructura de la data
 glimpse(train)
@@ -79,13 +83,13 @@ lematiza("Bad Bunny")
 
 # Tokenizaremos nuestros textos
 p_load(tidytext)
-tt$id <- 1:nrow(tt)
-tidy_ensayos <- tt %>%
+train$id <- 1:nrow(train)
+tidy_ensayos <- train %>%
   unnest_tokens(output = token, input = text)
 
 # Tenemos 13 mil palabras únicas. 
 # Esto no lo vamos a correr en la complementaria porque se demora mucho. Vamos a importar el diccionario ya melo
-diccionario_lemmatizador <- data.frame(corpus = unique(tidy_tt$token))
+diccionario_lemmatizador <- data.frame(corpus = unique(tidy_ensayos$token))
 
 diccionario_lemmatizador <- diccionario_lemmatizador %>% 
   # Si se hacen demasiadas querys nos van a bloquear entonces toca
@@ -96,54 +100,62 @@ diccionario_lemmatizador <- diccionario_lemmatizador %>%
 
 # Usando el comando automático de R con una aproximación más eficiente
 p_load(udpipe)
+library(udpipe)
 
 # Creamos el id de cada documento
-tt$id <- paste0("doc", 1:nrow(tt))
+train$id <- paste0("doc", 1:nrow(train))
 
 # Descargamos el modelo pre entrenado
-# udmodel <- udpipe_download_model(language = "spanish")
-# modelo <- udpipe_load_model(file = udmodel$file_model)
-file_model <- "C:/Users/User/Dropbox/BDML_Fall_2022_shared/04_Complementarias/14_LDA/spanish-gsd-ud-2.5-191206.udpipe"
-modelo <- udpipe_load_model(file = file_model)
-x <- udpipe_annotate(modelo, x = tt$text)
-tidy_tt <- as.data.frame(x)
+ udmodel <- udpipe_download_model(language = "spanish")
+ modelo <- udpipe_load_model(file = udmodel$file_model)
+#file_model <- "C:/Users/User/Dropbox/BDML_Fall_2022_shared/04_Complementarias/14_LDA/spanish-gsd-ud-2.5-191206.udpipe"
+#modelo <- udpipe_load_model(file = file_model)
+x <- udpipe_annotate(modelo, x = train$text)
+tidy_train <- as.data.frame(x)
 
 
-word_count <- tidy_tt %>%
+word_count <- tidy_train %>%
   group_by(doc_id) %>%
   count(lemma) %>%
   ungroup()
+table(word_count2$n)
 
 # Ahora vamos a eliminar las palabras extrañas (aquellas que aparezcan menos de 20 veces en los documentos) o demasiado comunes (que aparezcan en más del 50% de los documentos)
 word_count2 <- word_count %>%
   left_join(
-    word_count %>%
+     word_count %>%
       count(lemma) %>%
-      mutate(filtro = !((n <= 20) | (n >= 236*0.5))) %>%
+      mutate(filtro = !((n >= 6))) %>%
       select(-n)
   ) %>%
   filter(filtro) %>%
   select(-filtro)
 
+
+word_count$lemma<- ifelse(word_count$lemma == "t", NA, word_count$lemma)
+word_count$lemma<- ifelse(word_count$lemma == "co", NA, word_count$lemma)
+word_count$lemma<- ifelse(word_count$lemma == "https", NA, word_count$lemma)
+word_count = subset(x = word_count, subset = is.na(lemma)==FALSE)
+
 # Se nos colaron stopwords entonces otra vez chao
 # Normalizamos nuestros textos
 # Eliminamos los acentos
-word_count2$lemma <- stri_trans_general(str = word_count2$lemma, id = "Latin-ASCII")
+word_count$lemma <- stri_trans_general(str = word_count$lemma, id = "Latin-ASCII")
 # Quitamos stopwords
-filtro <- !(word_count2$lemma %in% stopwords_español)
-word_count2 <- word_count2[filtro, ]
+filtro <- !(word_count$lemma %in% stopwords_español)
+word_count <- word_count[filtro, ]
 
 p_load(tm, tidytext)
-ensayos_dtm <- cast_dtm(data = word_count2, 
+train_dtm <- cast_dtm(data = word_count, 
                         document = doc_id, 
                         term = lemma, 
                         value = n)
-inspect(tt_dtm)
+inspect(train_dtm)
 
 
 # Visualicemos las palabras más relevantes
 p_load(wordcloud)
-freq <- sort(colSums(as.matrix(tt_dtm)), 
+freq <- sort(colSums(as.matrix(train_dtm)), 
              decreasing = T)
 dev.new(width = 1000, height = 1000, unit = "px")
 wordcloud(names(freq), freq, max.words = 50,
@@ -153,17 +165,17 @@ wordcloud(names(freq), freq, max.words = 50,
 
 
 
-tt_dtm2 <- cast_dtm(data = word_count2, 
+train_dtm2 <- cast_dtm(data = word_count, 
                          document = doc_id, 
                          term = lemma, 
                          value = n,
                          weighting = tm::weightTfIdf)
-inspect(tt_dtm2)
+inspect(train_dtm2)
 
 
 
 # Visualicemos las palabras más relevantes
-freq2 <- sort(colSums(as.matrix(tt_dtm2)), 
+freq2 <- sort(colSums(as.matrix(train_dtm2)), 
               decreasing = T)
 dev.new(width = 1000, height = 1000, unit = "px")
 wordcloud(names(freq2), freq2, max.words = 50,
@@ -172,41 +184,43 @@ wordcloud(names(freq2), freq2, max.words = 50,
           scale = c(4, 0.5), rot.per = 0)
 
 
-X <- as.matrix(tt_dtm2)
+X <- as.matrix(train_dtm2)
 X_std <- (X - min(X)) / (max(X) - min(X))
 X_scaled <- X_std * (1000 - 0) + 0
 X_scaled <- round(X_scaled, 0)
 
+
+
 # Por toda esta gestión toca volver a hacer el proceso desde cero
-tt_dtm3 <- X_scaled %>%
+train_dtm3 <- X_scaled %>%
   as.data.frame() %>%
   rownames_to_column() %>%
   pivot_longer(-rowname) %>%
   cast_dtm(document = rowname,
            term = name, 
            value = value)
-inspect(tt_dtm3)
+inspect(train_dtm3)
 
 # Ahora estamos listos para usar LDA
 p_load(topicmodels)
-tt_lda <- LDA(tt_dtm3, k = 3, 
+train_lda <- LDA(train_dtm3, k = 3, 
                    control = list(seed = 666))
 
-tt_lda
+train_lda
 
 
-tt_topics <- tidy(tt_lda, matrix = "beta")
-tt_topics
+train_topics <- tidy(train_lda, matrix = "beta")
+train_topics
 
 
 
-tt_top_terms <- tt_topics %>%
+train_top_terms <- train_topics %>%
   group_by(topic) %>%
   slice_max(beta, n = 10) %>% 
   ungroup() %>%
   arrange(topic, -beta)
 
-tt_top_terms %>%
+train_top_terms %>%
   mutate(term = reorder_within(term, beta, topic)) %>%
   ggplot(aes(beta, term, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
